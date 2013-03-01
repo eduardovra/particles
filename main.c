52ef2ca86b7d68ac6fc0255b412012ebef0340fa
Eduardo Vieira <eduardovra@gmail.com>
2013-03-01 08:39:34 -0300

    DrawPixel identation fix.

--- main.c
+++ main.c
@@ -28,45 +28,45 @@ static st_velocity vel[NUM_OF_PARTICLES];
 
 void DrawPixel(SDL_Surface *screen, Uint8 R, Uint8 G, Uint8 B, Sint32 x, Sint32 y)
 {
-    Uint32 color = SDL_MapRGB(screen->format, R, G, B);
+	Uint32 color = SDL_MapRGB(screen->format, R, G, B);
 
-    switch (screen->format->BytesPerPixel) {
-        case 1: { /* Assuming 8-bpp */
-            Uint8 *bufp;
+	switch (screen->format->BytesPerPixel) {
+		case 1: { /* Assuming 8-bpp */
+			Uint8 *bufp;
 
-            bufp = (Uint8 *)screen->pixels + y*screen->pitch + x;
-            *bufp = color;
-        }
-        break;
+			bufp = (Uint8 *)screen->pixels + y*screen->pitch + x;
+			*bufp = color;
+		}
+		break;
 
-        case 2: { /* Probably 15-bpp or 16-bpp */
-            Uint16 *bufp;
+		case 2: { /* Probably 15-bpp or 16-bpp */
+			Uint16 *bufp;
 
-            bufp = (Uint16 *)screen->pixels + y*screen->pitch/2 + x;
-            *bufp = color;
-        }
-        break;
+			bufp = (Uint16 *)screen->pixels + y*screen->pitch/2 + x;
+			*bufp = color;
+		}
+		break;
 
-        case 3: { /* Slow 24-bpp mode, usually not used */
-            Uint8 *bufp;
+		case 3: { /* Slow 24-bpp mode, usually not used */
+			Uint8 *bufp;
 
-            bufp = (Uint8 *)screen->pixels + y*screen->pitch + x;
-            *(bufp+screen->format->Rshift/8) = R;
-            *(bufp+screen->format->Gshift/8) = G;
-            *(bufp+screen->format->Bshift/8) = B;
-        }
-        break;
+			bufp = (Uint8 *)screen->pixels + y*screen->pitch + x;
+			*(bufp+screen->format->Rshift/8) = R;
+			*(bufp+screen->format->Gshift/8) = G;
+			*(bufp+screen->format->Bshift/8) = B;
+		}
+		break;
 
-        case 4: { /* Probably 32-bpp */
-            Uint32 *bufp;
+		case 4: { /* Probably 32-bpp */
+			Uint32 *bufp;
 
-            bufp = (Uint32 *)screen->pixels + y*screen->pitch/4 + x;
-            *bufp = color;
-        }
-        break;
-    }
+			bufp = (Uint32 *)screen->pixels + y*screen->pitch/4 + x;
+			*bufp = color;
+		}
+		break;
+	}
 
-    SDL_UpdateRect(screen, x, y, 1, 1);
+	SDL_UpdateRect(screen, x, y, 1, 1);
 }
 
 uint64_t timespec_to_miliseconds(const struct timespec * ts)
